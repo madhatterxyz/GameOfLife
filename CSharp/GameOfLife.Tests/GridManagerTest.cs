00d43a13e261259b4278d15515307f199ffe6e15
Nicolas <Nicolas@PC-NICOLAS>
2022-11-09 13:42:26 +0100

    refactor domain and tests

--- CSharp/GameOfLife.Tests/GridManagerTest.cs
+++ CSharp/GameOfLife.Tests/GridManagerTest.cs
@@ -8,9 +8,8 @@ namespace GameOfLife.Tests
         [Fact]
         public void Should_not_change_cells_if_all_cells_are_dead()
         {
-            GridManager gridManager = new GridManager();
-            var grid = new Cell[1, 1];
-            var newGrid = gridManager.GetNewState(grid);
+            GridManager gridManager = new GridManager(1,1);
+            var newGrid = gridManager.GetNewState();
 
             Check.That(newGrid[0, 0].IsAlive).IsFalse();
         }
@@ -18,14 +17,12 @@ namespace GameOfLife.Tests
         [Fact]
         public void Should_not_kill_cell_if_it_has_two_neighbours()
         {
-            GridManager gridManager = new GridManager();
+            GridManager gridManager = new GridManager(3,3);
+            gridManager.Grid[0, 2].IsAlive = true;
+            gridManager.Grid[1, 1].IsAlive = true;
+            gridManager.Grid[2, 0].IsAlive = true;
 
-            Cell[,] grid = InitializeGrid(3, 3);
-            grid[0, 2].IsAlive = true;
-            grid[1, 1].IsAlive = true;
-            grid[2, 0].IsAlive = true;
-
-            var newGrid = gridManager.GetNewState(grid);
+            var newGrid = gridManager.GetNewState();
 
             Check.That(newGrid[1, 1].IsAlive).IsTrue();
         }
@@ -33,14 +30,12 @@ namespace GameOfLife.Tests
         [Fact]
         public void Should_kill_cell_if_it_has_less_than_two_neighbours()
         {
-            GridManager gridManager = new GridManager();
+            GridManager gridManager = new GridManager(2,4);
+            gridManager.Grid[1, 0].IsAlive = true;
+            gridManager.Grid[1, 1].IsAlive = true;
+            gridManager.Grid[1, 2].IsAlive = true;
 
-            Cell[,] grid = InitializeGrid(2, 4);
-            grid[1, 0].IsAlive = true;
-            grid[1, 1].IsAlive = true;
-            grid[1, 2].IsAlive = true;
-
-            var newGrid = gridManager.GetNewState(grid);
+            var newGrid = gridManager.GetNewState();
 
             Check.That(newGrid[1, 0].IsAlive).IsFalse();
             Check.That(newGrid[1, 2].IsAlive).IsFalse();
@@ -49,14 +44,12 @@ namespace GameOfLife.Tests
         [Fact]
         public void Should_alive_cell_if_it_has_three_neighbours()
         {
-            GridManager gridManager = new GridManager();
+            GridManager gridManager = new GridManager(2,3);
+            gridManager.Grid[0, 1].IsAlive = true;
+            gridManager.Grid[1, 0].IsAlive = true;
+            gridManager.Grid[1, 1].IsAlive = true;
 
-            Cell[,] grid = InitializeGrid(2, 3);
-            grid[0, 1].IsAlive = true;
-            grid[1, 0].IsAlive = true;
-            grid[1, 1].IsAlive = true;
-
-            var newGrid = gridManager.GetNewState(grid);
+            var newGrid = gridManager.GetNewState();
 
             Check.That(newGrid[0, 0].IsAlive).IsTrue();
         }
@@ -64,16 +57,14 @@ namespace GameOfLife.Tests
         [Fact]
         public void Should_kill_cell_if_it_has_more_than_three_neighbours()
         {
-            GridManager gridManager = new GridManager();
+            GridManager gridManager = new GridManager(4,3);
+            gridManager.Grid[0, 2].IsAlive = true;
+            gridManager.Grid[1, 1].IsAlive = true;
+            gridManager.Grid[2, 0].IsAlive = true;
+            gridManager.Grid[2, 1].IsAlive = true;
+            gridManager.Grid[2, 2].IsAlive = true;
 
-            Cell[,] grid = InitializeGrid(4, 3);
-            grid[0, 2].IsAlive = true;
-            grid[1, 1].IsAlive = true;
-            grid[2, 0].IsAlive = true;
-            grid[2, 1].IsAlive = true;
-            grid[2, 2].IsAlive = true;
-
-            var newGrid = gridManager.GetNewState(grid);
+            var newGrid = gridManager.GetNewState();
 
             Check.That(newGrid[1, 1].IsAlive).IsFalse();
         }
@@ -81,32 +72,17 @@ namespace GameOfLife.Tests
         [Fact]
         public void Should_count_neighbours_of_cell()
         {
-            GridManager gridManager = new GridManager();
 
             const int h = 2;
             const int w = 3;
-            Cell[,] grid = InitializeGrid(h, w);
-            grid[0, 1].IsAlive = true;
-            grid[1, 0].IsAlive = true;
-            grid[1, 1].IsAlive = true;
+            GridManager gridManager = new GridManager(h,w);
+            gridManager.Grid[0, 1].IsAlive = true;
+            gridManager.Grid[1, 0].IsAlive = true;
+            gridManager.Grid[1, 1].IsAlive = true;
 
-            int nbNeighbours = gridManager.CountNeighbours(grid, 0, 1);
+            int nbNeighbours = gridManager.CountNeighbours(0, 1);
 
             Check.That(nbNeighbours).IsEqualTo(2);
-        }
-
-        private static Cell[,] InitializeGrid(int h, int w)
-        {
-            var grid = new Cell[h, w];
-            for (int i = 0; i < grid.GetLength(0); i++)
-            {
-                for (int j = 0; j < grid.GetLength(1); j++)
-                {
-                    grid[i, j] = new Cell(false);
-                }
-            }
-
-            return grid;
         }
     }
 }
--- CSharp/GameOfLife/GridManager.cs
+++ CSharp/GameOfLife/GridManager.cs
@@ -2,27 +2,34 @@
 {
     public class GridManager
     {
-        public Cell[,] GetNewState(Cell[,] grid)
+        private const int NB_NEIGHBOURS_FOR_CELL_TO_BE_ALIVE = 3;
+        private const int NB_NEIGHBOURS_FOR_CELL_TO_SURVIVE = 2;
+        public Cell[,] Grid { get; set; }
+        public GridManager(int height, int width)
         {
-            var newGrid = new Cell[grid.GetLength(0), grid.GetLength(1)];
-            for (int i = 0; i < grid.GetLength(0); i++)
+            InitializeGrid(height,width);
+        }
+        public Cell[,] GetNewState()
+        {
+            var newGrid = new Cell[Grid.GetLength(0), Grid.GetLength(1)];
+            for (int i = 0; i < Grid.GetLength(0); i++)
             {
-                for (int j = 0; j < grid.GetLength(1); j++)
+                for (int j = 0; j < Grid.GetLength(1); j++)
                 {
                     newGrid[i, j] = new Cell(false);
                 }
             }
 
-            for (int i = 0; i < grid.GetLength(0); i++)
+            for (int i = 0; i < Grid.GetLength(0); i++)
             {
-                for (int j = 0; j < grid.GetLength(1); j++)
+                for (int j = 0; j < Grid.GetLength(1); j++)
                 {
-                    int nbNeighbours = CountNeighbours(grid, i, j);
-                    if (nbNeighbours == 2)
+                    int nbNeighbours = CountNeighbours( i, j);
+                    if (nbNeighbours == NB_NEIGHBOURS_FOR_CELL_TO_SURVIVE)
                     {
-                        newGrid[i, j].IsAlive = grid[i, j].IsAlive;
+                        newGrid[i, j].IsAlive = Grid[i, j].IsAlive;
                     }
-                    else if (nbNeighbours == 3)
+                    else if (nbNeighbours == NB_NEIGHBOURS_FOR_CELL_TO_BE_ALIVE)
                     {
                         newGrid[i, j].IsAlive = true;
                     }
@@ -31,52 +38,42 @@
             return newGrid;
         }
 
-        public int CountNeighbours(Cell[,] grid, int i, int j)
+        public int CountNeighbours(int i, int j)
         {
             int nbNeighbours = 0;
-            if (IsAliveNeighbour(grid, i - 1, j - 1))
+            for (int verticalIndex = -1; verticalIndex <= 1; verticalIndex++)
             {
-                nbNeighbours++;
-            }
-            if (IsAliveNeighbour(grid, i, j - 1))
-            {
-                nbNeighbours++;
-            }
-            if (IsAliveNeighbour(grid, i + 1, j - 1))
-            {
-                nbNeighbours++;
-            }
-            if (IsAliveNeighbour(grid, i - 1, j))
-            {
-                nbNeighbours++;
-            }
-            if (IsAliveNeighbour(grid, i + 1, j))
-            {
-                nbNeighbours++;
-            }
-            if (IsAliveNeighbour(grid, i - 1, j + 1))
-            {
-                nbNeighbours++;
-            }
-            if (IsAliveNeighbour(grid, i, j + 1))
-            {
-                nbNeighbours++;
-            }
-            if (IsAliveNeighbour(grid, i + 1, j + 1))
-            {
-                nbNeighbours++;
-            }
+                for (int horizontalIndex = -1; horizontalIndex <= 1; horizontalIndex++)
+                {
+                    if (!(verticalIndex == 0 && horizontalIndex == 0) &&
+                        IsAliveNeighbour( i + verticalIndex, j + horizontalIndex))
+                    {
+                        nbNeighbours++;
+                    }
 
+                }
+            }
             return nbNeighbours;
         }
 
-        private static bool IsAliveNeighbour(Cell[,] grid, int i, int j)
+        private bool IsAliveNeighbour(int i, int j)
         {
             return j >= 0 &&
-                j < grid.GetLength(1) &&
+                j < Grid.GetLength(1) &&
                 i >= 0 &&
-                i < grid.GetLength(0) &&
-                grid[i, j].IsAlive;
+                i < Grid.GetLength(0) &&
+                Grid[i, j].IsAlive;
+        }
+        private void InitializeGrid(int height, int width)
+        {
+            Grid = new Cell[height, width];
+            for (int i = 0; i < Grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < Grid.GetLength(1); j++)
+                {
+                    Grid[i, j] = new Cell(false);
+                }
+            }
         }
     }
 }
